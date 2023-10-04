d42949d71168bb553d45b7940ae85b9bd80e9645
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-10-04 17:41:19 +0000

    add comments

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -1,5 +1,14 @@
+----------------------
+-- simple functions --
+----------------------
+
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
     then x
     else doubleMe x
+
+-------------------------
+-- list comprehensions --
+-------------------------
+

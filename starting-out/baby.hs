9968d90ee5e904fe8551e0c613198565f1293566
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-10-04 20:57:43 +0000

    finish chapter 1

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -12,3 +12,9 @@ doubleSmallNumber x = if x > 100
 -- list comprehensions --
 -------------------------
 
+boomBangs xs = [ if x < 10 then "boom" else "bang" | x <- xs, odd x ]
+
+length' xs = sum [ 1 | _ <- xs ]
+
+-- let rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2] 
+-- let rightTriangles' = [ (a,b,c) | c <- [1..100], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24 ]

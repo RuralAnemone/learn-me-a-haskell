862ee7a8499411791864c366cf8ccad207c73f5b
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-10-06 20:12:29 +0000

    add benchmarking

--- types-n-typeclasses/toddler.hs
+++ types-n-typeclasses/toddler.hs
@@ -3,3 +3,5 @@ removeNonUppercase string = [ c | c <- string , c `elem` ['A'..'Z']]
 
 factorial :: Integer -> Integer
 factorial n = product [1..n]
+-- hello I've decided to benchmark this
+-- https://docs.google.com/spreadsheets/d/1o_9VSorpdDIxYOsgCKrlO8DOgIetDsxaCvNK4c1luIo/edit

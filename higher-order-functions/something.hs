925d8f6bd06593bbf17b565a637a6a079185cac8
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-12-15 16:40:09 -0600

    play with the linter

---
+++ higher-order-functions/something.hs
@@ -0,0 +1,16 @@
+largestDivisibleBy3829Under100000 :: (Integral a) => a
+largestDivisibleBy3829Under100000 = head (filter p [100000,99999..]) where
+  p x = x `mod` 3829 == 0
+
+sumOfAllOddSquaresLT10000 :: (Integral a) => a
+sumOfAllOddSquaresLT10000 = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | n < 1 = error "bozo"
+  | even n = n:chain (n `div` 2)
+  | odd n = n:chain (n*3 + 1)
+
+chainsLongerThan15'iUnder100 :: (Integral a) => a
+chainsLongerThan15'iUnder100 = 

ea28ad40cf119d8c962bdf47c135c56dae75679c
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-12-15 14:34:58 -0600

    dgkhsfljshLwq

---
+++ other/primes.hs
@@ -0,0 +1,5 @@
+-- from haskell.org
+primes = filterPrime [2 ..]
+  where
+    filterPrime (p : xs) =
+      p : filterPrime [x | x <- xs, x `mod` p /= 0]
--- types-n-typeclasses/toddler.hs
+++ types-n-typeclasses/toddler.hs
@@ -5,3 +5,7 @@ factorial :: Integer -> Integer
 factorial n = product [1..n]
 -- hello I've decided to benchmark this
 -- https://docs.google.com/spreadsheets/d/1o_9VSorpdDIxYOsgCKrlO8DOgIetDsxaCvNK4c1luIo/edit
+
+recursiveFactorial :: Integer -> Integer
+recursiveFactorial 0 = 1
+recursiveFactorial n = n * recursiveFactorial (n-1)

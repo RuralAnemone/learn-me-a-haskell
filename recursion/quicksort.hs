b12b7a2ef84cb4f3eca421b34fa7afec5be7d0b6
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-12-20 10:58:47 -0600

    readability

--- recursion/quicksort.hs
+++ recursion/quicksort.hs
@@ -1,6 +1,6 @@
 quickSort :: (Ord a) => [a] -> [a]
 quickSort [] = []
 quickSort (x : xs) =
-  let smallerSorted = quickSort [a | a <- xs, a < x]
-      biggerSorted = quickSort [a | a <- xs, a >= x]
+  let smallerSorted = quickSort $ filter (< x) xs
+      biggerSorted = quickSort $ filter (>= x) xs
    in smallerSorted ++ [x] ++ biggerSorted
